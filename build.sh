a30770692eb808ea217c9ddba32bbc9eaa378063
rufnx <chunaifimaruf@gmail.com>
2026-02-11 09:19:39 +0700

    scripts: build: drop zyc Clang 23

--- build.sh
+++ build.sh
@@ -2,23 +2,25 @@
 
 set -e
 
-KERNEL_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
-TOOLCHAIN=${KERNEL_DIR}/prebuilts
-GZIP=${KERNEL_DIR}/out/arch/arm64/boot/Image.gz
+KERNEL_DIR=$(cd -- $(dirname -- ${BASH_SOURCE[0]}) && pwd)
+TOOLCHAIN=$KERNEL_DIR/prebuilts
+GZIP=$KERNEL_DIR/out/arch/arm64/boot/Image.gz
 
-if [ ! -d ${TOOLCHAIN} ]; then
-    wget "$(curl -s https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-main-link.txt)" -O "zyc-clang.tar.gz"
-    mkdir ${TOOLCHAIN} && tar -xvf zyc-clang.tar.gz -C ${TOOLCHAIN}
-    ${TOOLCHAIN}/bin/clang --version
-    export PATH=${TOOLCHAIN}/bin:${PATH}
+[[ -z DEFCONFIG ]] && DEFCONFIG=rufnx_defconfig
+[[ -z BOT_TOKEN ]] && echo "BOT_TOKEN not set"
+[[ -z CHAT_ID ]] && echo "CHAT_ID not set"
+
+if [ ! -d $TOOLCHAIN ]; then
+    wget "$(curl -s https://raw.githubusercontent.com/ZyCromerZ/Clang/main/Clang-15-link.txt)" -O "zyc-clang.tar.gz"
+    mkdir $TOOLCHAIN && tar -xvf zyc-clang.tar.gz -C $TOOLCHAIN
+    $TOOLCHAIN/bin/clang --version
+    export PATH=$TOOLCHAIN/bin:$PATH
 fi
 
 ARGS=(
     -j$(nproc --all)
     O=out
     ARCH=arm64
-    LLVM=1
-    LLVM_IAS=1
     AR=llvm-ar
     NM=llvm-nm
     LD=ld.lld
@@ -26,17 +28,15 @@ ARGS=(
     OBJDUMP=llvm-objdump
     STRIP=llvm-strip
     CC=clang
-    DTC_EXT=dtc
     CROSS_COMPILE=aarch64-linux-gnu-
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     KCFLAGS=-w
     CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 )
 
-make ${ARGS[@]} rufnx_defconfig
+make ${ARGS[@]} $DEFCONFIG
 make ${ARGS[@]} | tee compile.log
 
-if [ -f ${GZIP} ]; then
+if [ -f $GZIP ]; then
     echo "##############" 
     echo "Build success!"
     echo "##############"
